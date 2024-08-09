83b8c8f16de3e8d7924e79b5a8101d95b47c3d29
PedroVidal27 <pedrovidal2703@gmail.com>
2024-08-09 11:45:43 -0300

    :wrench: setting up track player

--- app/index.tsx
+++ app/index.tsx
@@ -3,15 +3,22 @@ import styles from "./styles";
 import { pickDirectory } from "react-native-document-picker";
 import { useEffect, useState } from "react";
 import { useMusicList } from "@/hooks/useMusicList";
+import { MusicFile } from "@/types/MusicList";
+import { PERMISSIONS, request } from "react-native-permissions";
+import TrackPlayer from "react-native-track-player";
 
 export default function Index() {
   const [isLoaded, setIsLoaded] = useState<boolean>(false);
+  const [musicList, setMusicList] = useState<MusicFile[]>();
   const { getMusicList } = useMusicList();
+  TrackPlayer.registerPlaybackService(() => require('./service'));
   useEffect(() => {
     const loadPage = async () => {
+      await request(PERMISSIONS.ANDROID.READ_MEDIA_AUDIO)
       const response = await pickDirectory()
       if(!response) throw Error("Invalid directory");
-      console.log(await getMusicList(response.uri));
+      setMusicList(await getMusicList(response.uri));
+      setIsLoaded(true);
     };
 
     loadPage();
@@ -20,12 +27,12 @@ export default function Index() {
     <View style={styles.container}>
       <SectionList
         style={{ opacity: isLoaded ? 1 : 0 }}
-        sections={[{ title: "A", data: ["Alabama", "Acre", "Almeida"] }]}
-        renderItem={({ item }) => <Text style={styles.musicTitle}>{item}</Text>}
+        sections={[{ title: "Música", data: musicList ? musicList : [] }]}
+        renderItem={({ item }) => <Text style={styles.musicTitle}>{item.title}</Text>}
         renderSectionHeader={({ section }) => (
-          <Text style={styles.sectionTitle}>{section.title}</Text>
+          <Text onPress={() => {}} style={styles.sectionTitle}>{section.title}</Text>
         )}
-        keyExtractor={(item) => `music-${item}`}
+        keyExtractor={(item) => `music-${item.uri}`}
       />
     </View>
   );
--- hooks/useMusicList.ts
+++ hooks/useMusicList.ts
@@ -2,31 +2,34 @@ import { MusicFile } from "@/types/MusicList";
 import RNFS from "react-native-fs";
 
 export const useMusicList = () => {
-    const formatUri = (uri: string) => {
-        const uriComponents = uri.split("%20").join("\ ").split("%3A");
-        const fileNameAndExtension = uriComponents[uriComponents.length - 1]
-        return `${RNFS.ExternalStorageDirectoryPath}/${fileNameAndExtension}`;
+  const formatUri = (uri: string) => {
+    const uriComponents = uri.split("%20").join(" ").split("%3A");
+    const fileNameAndExtension = uriComponents[uriComponents.length - 1];
+    return `${RNFS.ExternalStorageDirectoryPath}/${fileNameAndExtension}`;
+  };
+  const getMusicList = async (uri: string): Promise<MusicFile[]> => {
+    const contentData = { uri };
+    if (contentData.uri.startsWith("content://"))
+      contentData.uri = formatUri(contentData.uri);
+    const musicList: MusicFile[] = [];
+    const files = await RNFS.readDir(contentData.uri);
+    for (const file of files) {
+      if (file.isDirectory()) {
+        musicList.push(...await getMusicList(file.path));
       }
-    const getMusicList = async (uri: string): Promise<MusicFile[]> => {
-        const contentData = { uri }
-        if (contentData.uri.startsWith('content://')) contentData.uri = formatUri(contentData.uri)
-        const musicList: MusicFile[] = []
-        const files = await RNFS.readDir(contentData.uri);
-        for(const file of files){
-          if(file.isDirectory()) musicList.concat(await getMusicList(file.path));
-          const fileNameComponents = file.name.split('.')
-          const extensionName = fileNameComponents[fileNameComponents.length - 1];
-          if(["mp3", "m4a"].indexOf(extensionName)) {
-            const music = {
-              uri: file.path,
-              title: fileNameComponents[0]
-            }
-            musicList.push(music)
-          }
-        }
-        return musicList;
-      };
-    return {
-        getMusicList
+      const fileNameComponents = file.name.split(".");
+      const extensionName = fileNameComponents[fileNameComponents.length - 1];
+      if (["mp3", "m4a"].indexOf(extensionName) != -1) {
+        const music = {
+          uri: file.path,
+          title: file.name,
+        };
+        musicList.push(music);
+      }
     }
-}
+    return musicList;
+  };
+  return {
+    getMusicList,
+  };
+};
