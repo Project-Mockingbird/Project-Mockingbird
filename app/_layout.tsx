54d77c3db5d4c20243b6c26aad1be4f33ee97f20
PedroVidal27 <pedrovidal2703@gmail.com>
2024-08-10 14:30:43 -0300

    :bug: fix routing

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,16 @@
-import { Tabs } from "expo-router";
+import { Stack, Tabs } from "expo-router";
 import { AppRegistry } from "react-native";
-import {expo} from "../app.json";
+import { expo } from "../app.json";
 
 AppRegistry.registerComponent(expo.name, () => App);
 
 export default function App() {
   return (
-    <Tabs initialRouteName="config">
-      <Tabs.Screen name="library"/>
-      <Tabs.Screen name="config"/>
-    </Tabs>
+    <Stack
+      screenOptions={{
+        animation: "none",
+        headerShown: false,
+      }}
+    />
   );
 }
