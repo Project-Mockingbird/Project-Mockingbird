f91067aaa6117243ca6ce30c944f2b497c5c10fa
PedroVidal27 <pedrovidal2703@gmail.com>
2024-08-10 15:45:11 -0300

    :sparkles: add music card

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,8 +1,21 @@
 import { Tabs } from "expo-router";
 
 export default function MainRouter() {
-  return <Tabs>
-    <Tabs.Screen name="library" />
-    <Tabs.Screen name="config" />
-  </Tabs>;
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="library"
+        options={{
+          title: "Library",
+          headerTitle: "Your Library",
+        }}
+      />
+      <Tabs.Screen
+        name="config"
+        options={{
+          title: "Settings",
+        }}
+      />
+    </Tabs>
+  );
 }
---
+++ src/components/music-card/styles.ts
@@ -0,0 +1,22 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    width: 150,
+  },
+  albumImage: {
+    borderRadius: 10,
+    height: 150,
+    aspectRatio: 1 / 1,
+  },
+  cardName: {
+    fontSize: 18,
+    color: "white",
+  },
+  lowerCardInfo: {
+    fontSize: 12,
+    color: "#d3d3d3",
+  },
+});
+
+export default styles;
