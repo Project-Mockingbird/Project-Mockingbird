377774028900d3ea80a09472bbc71584857d8625
PedroVidal27 <pedrovidal2703@gmail.com>
2024-08-10 01:46:07 -0300

    :poop: it plays music

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,10 @@
 import { Stack } from "expo-router";
+import { AppRegistry } from "react-native";
+import {expo} from "../app.json";
 
-export default function RootLayout() {
+AppRegistry.registerComponent(expo.name, () => App);
+
+export default function App() {
   return (
     <Stack>
       <Stack.Screen name="index" />
--- app/index.tsx
+++ app/index.tsx
@@ -5,32 +5,52 @@ import { useEffect, useState } from "react";
 import { useMusicList } from "@/hooks/useMusicList";
 import { MusicFile } from "@/types/MusicList";
 import { PERMISSIONS, request } from "react-native-permissions";
-import TrackPlayer from "react-native-track-player";
+import { addTracks, setupPlayer } from "@/services/trackplayer";
+import TrackPlayer, { AddTrack } from "react-native-track-player";
 
 export default function Index() {
   const [isLoaded, setIsLoaded] = useState<boolean>(false);
+  const [isPlayerReady, setIsPlayerReady] = useState(false);
   const [musicList, setMusicList] = useState<MusicFile[]>();
   const { getMusicList } = useMusicList();
-  TrackPlayer.registerPlaybackService(() => require('./service'));
+
   useEffect(() => {
     const loadPage = async () => {
-      await request(PERMISSIONS.ANDROID.READ_MEDIA_AUDIO)
-      const response = await pickDirectory()
-      if(!response) throw Error("Invalid directory");
+      await request(PERMISSIONS.ANDROID.READ_MEDIA_AUDIO);
+      const response = await pickDirectory();
+      if (!response) throw Error("Invalid directory");
       setMusicList(await getMusicList(response.uri));
+      let isSetup = await setupPlayer();
+
+      setIsPlayerReady(isSetup);
       setIsLoaded(true);
     };
 
     loadPage();
   }, []);
+
+  const addSong = (song: MusicFile) => {
+    addTracks([{ url: song.uri }]);
+    return;
+  };
   return (
     <View style={styles.container}>
       <SectionList
         style={{ opacity: isLoaded ? 1 : 0 }}
         sections={[{ title: "Música", data: musicList ? musicList : [] }]}
-        renderItem={({ item }) => <Text style={styles.musicTitle}>{item.title}</Text>}
+        renderItem={({ item }) => (
+          <Text
+            onPress={() => {
+              console.log(item);
+              addSong(item);
+            }}
+            style={styles.musicTitle}
+          >
+            {item.title}
+          </Text>
+        )}
         renderSectionHeader={({ section }) => (
-          <Text onPress={() => {}} style={styles.sectionTitle}>{section.title}</Text>
+          <Text style={styles.sectionTitle}>{section.title}</Text>
         )}
         keyExtractor={(item) => `music-${item.uri}`}
       />
